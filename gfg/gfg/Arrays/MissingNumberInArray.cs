a9a0ec7150467ef6334b8f70909a4860de5b8ad6
Shashank Shekhar <shekharshashank@outlook.com>
2022-05-21 19:13:25 +0530

    more probs

---
+++ gfg/gfg/Arrays/MissingNumberInArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gfg.Arrays
+{
+    class MissingNumberInArray
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int MissingNumber(int[] array, int n)
+        {
+            int sum = n * (n - 1) / 2;
+            int sum1 = 0;
+            for(int i=0; i<array.Length;i++)
+            {
+                sum1 += array[i];
+            }
+
+            return sum - sum1;
+        }
+    }
+}
---
+++ gfg/gfg/Arrays/SubArrayWithGivenSum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gfg.Arrays
+{
+    class SubArrayWithGivenSum
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 8 };
+            int n = 1;
+            int s = 8;
+            Console.WriteLine(string.Join(",",SubarraySum(arr, n, s)));
+        }
+
+        static List<int> SubarraySum(int[] arr, int n, int s)
+        {
+            if (n == 0) return new List<int>() { -1 };
+            if(n == 1 && arr[0] != s) return new List<int>() { -1 };
+            if (n == 1 && arr[0] == s) return new List<int>() { 1,1 };
+            int i = 0; int j = 1;
+
+            int sum = arr[i];
+            while(j < n)
+            {
+                sum = sum  + arr[j];
+                if (sum == s) return new List<int>() { i+1, j+1 };
+
+                if (sum < s) j++;
+                else 
+                {
+                    
+                    i++;
+                   
+                    sum = arr[i];
+                    if (sum == s) return new List<int>() { i + 1, i + 1 };
+                    j = i+1;
+                }
+            }
+            return new List<int>() { -1 };
+        }
+    }
+}
