5f7dc31efe814132d09e45d894b81eff17fce8bf
Shashank Shekhar <shekharshashank@outlook.com>
2022-05-23 00:39:24 +0530

    more probs

--- gfg/gfg/Arrays/LargestSubArrayWithSumZero.cs
+++ gfg/gfg/Arrays/LargestSubArrayWithSumZero.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,41 @@ namespace gfg.Arrays
     {
         static void Main(string[] args)
         {
-
+            int[] arr = new int[] { -1, 1, - 1, 1 };
+            int n = 4;
+            Console.WriteLine(MaxLen(arr, n));
         }
 
         static int MaxLen(int[] arr, int n)
         {
-            // Your code here
+            if (arr == null || arr.Length == 0) return -1;
+            int sum = 0;
+            int maxLen = 0;
+
+            
+            var dict = new Hashtable();
+
+            for (int i = 0; i < arr.Length; i++)
+            {   
+                sum += arr[i];
+
+                if (arr[i] == 0 && maxLen == 0) maxLen = 1;
+
+                if (sum == 0)
+                    maxLen = i + 1;
+
+                if (dict.ContainsKey(sum)) // if sum already exists then there would have been subarray that would have contributed to zero sum
+                {
+                    maxLen = Math.Max(maxLen, i - (int)dict[sum]);
+                }
+                else
+                {
+                    dict.Add(sum, i);
+                }
+
+            }
+
+            return maxLen;
         }
     }
 }
